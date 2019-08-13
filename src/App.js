66680ae583be3068d6676ae6451410acc631ea36
Jeremy Kithome <jeremy.kithome@andela.com>
2019-08-14 00:30:50 +0300

    feat: add audio recording button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {
+  Container,
+  Menu,
+  Grid,
+  Button,
+  Icon
+} from 'semantic-ui-react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [recording, setRecording] = useState(false);
+
+  const toggleRecording = () => {
+    setRecording(!recording);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu fixed='top' inverted>
+        <Container>
+          <Menu.Item as='a' header>
+            Audio Visualisation With React
+          </Menu.Item>
+        </Container>
+      </Menu>
+      <Container style={{ marginTop: '7em' }}>
+        <Grid verticalAlign='middle' centered columns={2}>
+          <Grid.Column style={{ display: 'flex', alignItems: 'center' }}>
+            <Button
+              color={recording ? 'red' : 'green'}
+              icon
+              labelPosition='left'
+              onClick={toggleRecording}
+            >
+              <Icon name={recording ? 'stop' : 'play'} />
+              {recording ? 'Stop' : 'Record'}
+            </Button>
+          </Grid.Column>
+        </Grid>
+      </Container>
     </div>
   );
 }
-
 export default App;
